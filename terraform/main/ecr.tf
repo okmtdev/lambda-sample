7db7f439b487a663d5980e018d2f8dd28a979f75
okmtdev <okmtdev@gmail.com>
2024-09-15 21:34:23 +0900

    minor update

--- terraform/main/ecr.tf
+++ terraform/main/ecr.tf
@@ -1,7 +1,9 @@
-resource "aws_ecr_repository" "lambda_sample" {
-  name                 = "lambda-sample"
+resource "aws_ecr_repository" "lambda_sample_ecr" {
+  name                 = "lambda-sample-ecr"
   image_tag_mutability = "MUTABLE"
+  force_delete         = false
 
+  # https://docs.aws.amazon.com/AmazonECR/latest/userguide/image-scanning.html
   image_scanning_configuration {
     scan_on_push = true
   }

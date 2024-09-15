be7635b71dc71e10ced14a033eee3ec34e9fc0e3
okmtdev <okmtdev@gmail.com>
2024-09-16 00:28:16 +0900

    add github action

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -2,3 +2,35 @@ module "ecr" {
   source = "../modules/ecr"
   name   = "lambda-sample-ecr"
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_lambda_function" "function" {
+  function_name = "lambda-sample-function"
+  role          = aws_iam_role.lambda.arn
+  image_uri     = "${module.ecr.repository.repository_url}:latest"
+
+  timeouts {
+    create = "15m"
+  }
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "lambda-sample-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}

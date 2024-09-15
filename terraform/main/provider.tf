7d08d97ec01308535a1ca75d2a2cb5f7aeb6ea15
okmtdev <okmtdev@gmail.com>
2024-09-15 21:20:56 +0900

    add tags

--- terraform/main/provider.tf
+++ terraform/main/provider.tf
@@ -2,12 +2,19 @@ terraform {
   required_version = ">= 1.6.3"
   backend "s3" {
     bucket  = "happy-wedding-tfstate"
-    region  = "ap-northeast-1"
+    region  = var.aws_region
     key     = "terraform.tfstate"
     encrypt = true
   }
 }
 
 provider "aws" {
-  region = "ap-northeast-1"
+  region = var.aws_region
+  default_tags {
+    tags = {
+      service   = var.service_name
+      owner     = "okmtdev"
+      terraform = "true"
+    }
+  }
 }

6ad6844a50d7fb5576871ad715270b2a391e89e5
okmtdev <okmtdev@gmail.com>
2024-09-16 18:06:45 +0900

    minor update

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -69,7 +69,7 @@ resource "aws_lambda_permission" "api_url" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda" {
-  role       = aws_iam_role.lambda-sample-role.name
+  role       = aws_iam_role.api.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 

3faf110b6cdf764bea2f958abce67718888a4df8
okmtdev <okmtdev@gmail.com>
2024-09-16 11:18:03 +0900

    minor update

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -14,6 +14,11 @@ resource "aws_lambda_function" "function" {
   }
 }
 
+resource "aws_lambda_function_url" "function-url" {
+  function_name      = aws_lambda_function.function.function_name
+  authorization_type = "NONE"
+}
+
 resource "aws_iam_role" "lambda-sample-role" {
   name = "lambda-sample-role"
 
@@ -37,4 +42,28 @@ EOF
 resource "aws_iam_role_policy_attachment" "lambda" {
   role       = aws_iam_role.lambda-sample-role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+
+resource "aws_apigatewayv2_api" "api" {
+  name          = "lambda-sample-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.function.invoke_arn
+}
+
+resource "aws_apigatewayv2_route" "route" {
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = "ANY /"
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "stage" {
+  api_id      = aws_apigatewayv2_api.api.id
+  name        = "$default"
+  auto_deploy = true
 }
